58e506cc59e6d52beadfe174f48eb0a8d6fbd277
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-05-24 02:41:11 +0530

    new build

--- OmegaBluetoothController/OmegaLEController.h
+++ OmegaBluetoothController/OmegaLEController.h
@@ -10,7 +10,7 @@
  * File Created: Saturday, 18th May 2024 10:28:57 pm
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
- * Last Modified: Friday, 24th May 2024 12:39:21 am
+ * Last Modified: Friday, 24th May 2024 2:31:41 am
  * Modified By: Omegaki113r (omegaki113r@gmail.com)
  * -----
  * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
@@ -39,6 +39,7 @@ extern "C"
 #include "esp_bt_main.h"
 #include "esp_gap_ble_api.h"
 #include "esp_gatt_common_api.h"
+#include "esp_gattc_api.h"
 #include "esp_gatts_api.h"
 
 #define CHAR2INT(x) (('0' <= x && x <= '9') ? (x - '0') : (('a' <= x && x <= 'f') ? (10 + (x - 'a')) : (('A' <= x && x <= 'F') ? (10 + (x - 'A')) : (0))))
