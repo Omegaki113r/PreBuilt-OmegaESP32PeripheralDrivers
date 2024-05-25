7958f22fd252b440e660191194b951bd0b79b6e4
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-05-26 01:24:39 +0530

    build with advertising parameter change APIs

--- OmegaBluetoothController/OmegaLEController.h
+++ OmegaBluetoothController/OmegaLEController.h
@@ -10,7 +10,7 @@
  * File Created: Saturday, 18th May 2024 10:28:57 pm
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
- * Last Modified: Saturday, 25th May 2024 12:10:05 am
+ * Last Modified: Sunday, 26th May 2024 12:53:02 am
  * Modified By: Omegaki113r (omegaki113r@gmail.com)
  * -----
  * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
@@ -150,8 +150,22 @@ extern "C"
 
       LEControllerStatus OmegaLEController_add_gatts_profile(ProfileHandle in_profile);
       LEControllerStatus OmegaLEController_set_device_name(const char *in_device_name);
-      LEControllerStatus OmegaLEController_set_advertising_device_name(const char *in_device_name);
-      LEControllerStatus OmegaLEController_disable_advertising_device_name(bool in_device_name_enabled);
+      LEControllerStatus OmegaLEController_change_advertising_device_name_state(bool in_enable_device_name);
+      LEControllerStatus OmegaLEController_change_scan_response_device_name_state(bool in_enable_device_name);
+      LEControllerStatus OmegaLEController_change_advertising_tx_power_state(bool in_enable_tx_power);
+      LEControllerStatus OmegaLEController_change_scan_response_tx_power_state(bool in_enable_tx_power);
+
+      LEControllerStatus OmegaLEController_change_advertising_service_data(const uint8_t *in_service_data_buffer, const uint16_t in_service_data_buffer_length);
+      LEControllerStatus OmegaLEController_change_scan_response_service_data(const uint8_t *in_service_data_buffer, const uint16_t in_service_data_buffer_length);
+      LEControllerStatus OmegaLEController_change_advertising_manufacturer_data(const uint8_t *in_manufacturer_data_buffer, const uint16_t in_manufacturer_data_buffer_length);
+      LEControllerStatus OmegaLEController_change_scan_response_manufacturer_data(const uint8_t *in_manufacturer_data_buffer, const uint16_t in_manufacturer_data_buffer_length);
+
+      LEControllerStatus OmegaLEController_set_advertising_service_uuid16(uint16_t in_service_uuid);
+      LEControllerStatus OmegaLEController_set_advertising_service_uuid32(uint32_t in_service_uuid);
+      LEControllerStatus OmegaLEController_set_advertising_service_uuid128(uint8_t in_service_uuid[UUID128]);
+      LEControllerStatus OmegaLEController_set_scan_response_service_uuid16(uint16_t in_service_uuid);
+      LEControllerStatus OmegaLEController_set_scan_response_service_uuid32(uint32_t in_service_uuid);
+      LEControllerStatus OmegaLEController_set_scan_response_service_uuid128(uint8_t in_service_uuid[UUID128]);
       LEControllerStatus OmegaLEController_start_advertising();
       LEControllerStatus OmegaLEController_stop_advertising();
       LEControllerStatus OmegaLEController_init_LE();
