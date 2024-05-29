06c34baf242989cd50e7c929d5210dd8fd86a7c4
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-05-30 03:54:36 +0530

    adding API to
    OmegaWiFiController_set_sta_provisioning
    OmegaWiFiController_set_station_status_changed_callback

--- OmegaWiFiController/OmegaWiFiController.h
+++ OmegaWiFiController/OmegaWiFiController.h
@@ -1,3 +1,27 @@
+/**
+ * @file OmegaWiFiController.h
+ * @author Omegaki113r
+ * @date Monday, 13th May 2024 4:07:25 pm
+ * @copyright Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * */
+/*
+ * Project: OmegaESP32PeripheralDrivers
+ * File Name: OmegaWiFiController.h
+ * File Created: Monday, 13th May 2024 4:07:25 pm
+ * Author: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Last Modified: Thursday, 30th May 2024 3:39:22 am
+ * Modified By: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ *
+ * 29-05-2024	0m3g4	initial developemt started
+ */
+
 #ifndef __OMEGA_WIFI_CONTROLLER_H__
 #define __OMEGA_WIFI_CONTROLLER_H__
 
@@ -14,6 +38,14 @@ extern "C"
 
     typedef enum
     {
+        STA_CONNECTED,
+        STA_DISCONNECTED,
+        STA_CONNECTION_FAILED,
+        STA_GOT_IP,
+    } StationStatus;
+
+    typedef enum
+    {
         StationMode,
         AccessPointMode,
         DualMode,
@@ -26,19 +58,18 @@ extern "C"
     } AccessPoint_t;
 
     typedef uint64_t AccessPointHandle;
-    typedef void (*sta_connected_cb_t)(void);
-    typedef void (*sta_disconnected_cb_t)(void);
-    typedef void (*sta_got_ip_cb_t)(void);
+    typedef void (*sta_status_changed_cb_t)(StationStatus);
     typedef void (*ap_assigned_ip_cb_t)(void);
     typedef void (*scan_result_cb_t)(AccessPoint_t *, size_t);
 
     WiFiControllerStatus OmegaWiFiController_initialize(WiFiControllerMode in_mode);
     WiFiControllerStatus OmegaWiFiController_deinitialize();
-    WiFiControllerStatus OmegaWiFiController_set_sta_provisioning(const char *in_ssid, const char *in_password);
+    WiFiControllerStatus OmegaWiFiController_set_sta_provisioning(const char *in_ssid, const char *in_password, uint16_t in_retry_count);
     WiFiControllerStatus OmegaWiFiController_set_ap_provisioning(const char *in_ssid, const char *in_password);
     WiFiControllerStatus OmegaWiFiController_start();
     WiFiControllerStatus OmegaWiFiController_start_scan();
     WiFiControllerStatus OmegaWiFiController_set_scan_result_callback(scan_result_cb_t in_scan_result_cb);
+    WiFiControllerStatus OmegaWiFiController_set_station_status_changed_callback(sta_status_changed_cb_t in_station_status_cb);
 
 #ifdef __cplusplus
 }
