997b2e672393d54a383061d40f28875f29d0d828
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-05-31 04:47:53 +0530

    build with basic incomplete config with json

--- OmegaWiFiController/OmegaWiFiController.h
+++ OmegaWiFiController/OmegaWiFiController.h
@@ -10,7 +10,7 @@
  * File Created: Monday, 13th May 2024 4:07:25 pm
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
- * Last Modified: Friday, 31st May 2024 1:02:05 am
+ * Last Modified: Friday, 31st May 2024 3:51:10 am
  * Modified By: Omegaki113r (omegaki113r@gmail.com)
  * -----
  * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
@@ -18,9 +18,9 @@
  * HISTORY:
  * Date      	By	Comments
  * ----------	---	---------------------------------------------------------
- * 
+ *
  * 31-05-2024	0m3g4	adding dual mode
- * 
+ *
  * 30-05-2024	0m3g4	adding access point feature
  *
  * 30-05-2024	0m3g4	adding OmegaWiFiController_set_sta_provisioning,OmegaWiFiController_set_station_status_changed_callback APIs
@@ -59,9 +59,10 @@ extern "C"
 
     typedef enum
     {
-        StationMode,
-        AccessPointMode,
-        DualMode,
+        Invalid,
+        StationMode = (1 << 0),
+        AccessPointMode = (1 << 1),
+        DualMode = (StationMode | AccessPointMode),
     } WiFiControllerMode;
 
     typedef struct
@@ -75,11 +76,11 @@ extern "C"
     typedef void (*ap_status_changed_cb_t)(APStatus);
     typedef void (*scan_result_cb_t)(AccessPoint_t *, size_t);
 
+    WiFiControllerStatus OmegaWiFiController_config(const char *in_json_str);
     WiFiControllerStatus OmegaWiFiController_initialize(WiFiControllerMode in_mode);
     WiFiControllerStatus OmegaWiFiController_deinitialize();
     WiFiControllerStatus OmegaWiFiController_set_sta_provisioning(const char *in_ssid, const char *in_password, uint16_t in_retry_count);
     WiFiControllerStatus OmegaWiFiController_set_ap_provisioning(const char *in_ssid, const char *in_password);
-    WiFiControllerStatus OmegaWiFiController_start();
     WiFiControllerStatus OmegaWiFiController_start_scan();
     WiFiControllerStatus OmegaWiFiController_set_scan_result_callback(scan_result_cb_t in_scan_result_cb);
     WiFiControllerStatus OmegaWiFiController_set_station_status_changed_callback(sta_status_changed_cb_t in_station_status_cb);
