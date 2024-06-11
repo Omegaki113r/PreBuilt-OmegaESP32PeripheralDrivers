a19ef8b8792f9cf31dffca40500dbce2f8bd028d
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-06-11 12:20:23 +0530

    added StationStatusInformation_t added station_state_changed added OmegaWiFiController_get_current_connected_station

--- OmegaWiFiController/OmegaWiFiController.h
+++ OmegaWiFiController/OmegaWiFiController.h
@@ -10,7 +10,7 @@
  * File Created: Monday, 13th May 2024 4:07:25 pm
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
- * Last Modified: Saturday, 1st June 2024 8:37:49 pm
+ * Last Modified: Tuesday, 11th June 2024 12:20:06 pm
  * Modified By: Omegaki113r (omegaki113r@gmail.com)
  * -----
  * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
@@ -18,6 +18,10 @@
  * HISTORY:
  * Date      	By	Comments
  * ----------	---	---------------------------------------------------------
+ *
+ * 11-06-2024	0m3g4	added StationStatusInformation_t
+                        added station_state_changed
+                        added OmegaWiFiController_get_current_connected_station
  *
  * 01-06-2024	0m3g4	build with __attribute__((weak)) for user callback for access points and scan results
  *
@@ -52,6 +56,7 @@ extern "C"
 
     typedef enum
     {
+        STA_CONNECTING,
         STA_CONNECTED,
         STA_DISCONNECTED,
         STA_CONNECTION_FAILED,
@@ -79,13 +84,19 @@ extern "C"
         uint8_t m_mac_address[8];
     } AccessPoint_t;
 
+    typedef struct
+    {
+        StationStatus status;
+        char ssid[33];
+    } StationStatusInformation_t;
+
     typedef uint64_t AccessPointHandle;
     typedef void (*sta_status_changed_cb_t)(StationStatus);
     typedef void (*ap_status_changed_cb_t)(APStatus);
     typedef void (*scan_result_cb_t)(AccessPoint_t *, size_t);
 
     __attribute__((weak)) void scan_result_callback(AccessPoint_t *, size_t);
-    __attribute__((weak)) void station_state_changed(StationStatus);
+    __attribute__((weak)) void station_state_changed(StationStatusInformation_t);
     __attribute__((weak)) void access_point_state_changed(APStatus);
 
     WiFiControllerStatus OmegaWiFiController_manual_config(const char *in_json_str);
@@ -98,6 +109,7 @@ extern "C"
     WiFiControllerStatus OmegaWiFiController_set_scan_result_callback(scan_result_cb_t in_scan_result_cb);
     WiFiControllerStatus OmegaWiFiController_set_station_status_changed_callback(sta_status_changed_cb_t in_station_status_cb);
     WiFiControllerStatus OmegaWiFiController_set_access_point_status_changed_callback(ap_status_changed_cb_t in_access_point_status_cb);
+    StationStatusInformation_t OmegaWiFiController_get_current_connected_station();
 
 #ifdef __cplusplus
 }
