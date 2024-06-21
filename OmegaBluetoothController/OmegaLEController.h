9eb39e02f46b0c32395e461b479437228c545d6b
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-06-21 14:24:29 +0530

    SPIRAM compatibility added minor logging added

--- OmegaBluetoothController/OmegaLEController.h
+++ OmegaBluetoothController/OmegaLEController.h
@@ -10,7 +10,7 @@
  * File Created: Saturday, 18th May 2024 10:28:57 pm
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
- * Last Modified: Sunday, 9th June 2024 5:39:26 pm
+ * Last Modified: Friday, 21st June 2024 2:10:57 pm
  * Modified By: Omegaki113r (omegaki113r@gmail.com)
  * -----
  * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
